cb7cece89a16e228bc611a595e998bea6078afa9
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2018-11-12 08:46:19 -0200

    Do something according to intent from Watson

--- requests.js
+++ requests.js
@@ -2,6 +2,16 @@ module.exports = {
     identifyIntent: identifyIntent = (intent) => {
         console.log(intent)
     },
+    actionIntent: actionIntent = (intent, context) => {
+        switch(intent) {
+            case 'consultRequest':
+            consultRequest(context);
+            break;
+            case 'cancelRequest':
+            cancelRequest(context);
+            break;
+        }
+    },
     consultRequest: consultRequest = () => {
         console.log('Calling consult request')
     },
